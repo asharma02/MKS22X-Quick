f38fed84b806016f911e0f8c3b175006a3e33afb
Aryan Sharma <asharma00@stuy.edu>
2019-03-12 20:56:39 -0400

    added comments

--- Quick.java
+++ Quick.java
@@ -13,17 +13,17 @@ public class Quick {
  *4. all elements in range that are larger than the pivot element are placed after the pivot element.
  *@return the index of the final position of the pivot element.
  */
-public static int partition ( int [] data, int start, int end){
-  int pivot = end;
-  int part = start;
-  for (int i = start; i <= end - 1; i++) {
-    if (data[i] < data[pivot]) {
-      swap(data, i, part);
-      part++;
+public static int partition (int [] data, int start, int end){
+  int pivot = end;//set pivot to one side
+  int part = start; //set start index
+  for (int i = start; i <= end - 1; i++) { //loop through array
+    if (data[i] < data[pivot]) { //if the current is less than the pivot
+      swap(data, i, part);//swap the current with the start
+      part++; //add to the partition
     }
   }
-  swap(data, part, pivot);
-  return part;
+  swap(data, part, pivot); //switch the pivot back to the partition
+  return part; //this will be the pivot that was moved
 }
 
 //private method to swap two indicies
@@ -36,24 +36,25 @@ private static void swap(int[] data, int first, int second) {
 /*return the value that is the kth smallest value of the dataay.
  */
  public static int quickselect(int []data, int k){
-   return quickselecth(data, 0, data.length - 1, k);
+   return quickselecth(data, 0, data.length - 1, k); //call helper method with start = 0 and end = last index
  }
 
  private static int quickselecth(int[] data, int start, int end, int k) {
    if (start == end) {
             return data[start];
-    }
-    int pivot = partition(data, start, end);
+    } //if the start and end are the same, return that value
+    int pivot = partition(data, start, end); //partition it
     if (pivot == k) {
       return data[pivot];
-    }
+    } //when the pivot is the same as k, return the value at the pivot
     if (k < pivot) {
       return quickselecth(data, start, pivot - 1, k);
-    }
+    } //if it is less, look at the left side of the of the pivot
     else {
-      return quickselecth(data, pivot + 1, end, k);
+      return quickselecth(data, pivot + 1, end, k); //if it is greater, look at the right side of the pivot
     }
  }
+
 
 
  public static void main(String[] args) {
